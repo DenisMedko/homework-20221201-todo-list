e866e8e27101cf04e9b86da9c8df526e2629f030
DenisMedko <medko.denis76@gmail.com>
2022-12-04 14:12:52 +0200

    actionCreators refactored

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,14 +1,14 @@
 import ACTION_TYPES from './actionTypes';
 
-export const changeItemTitle = value => 
-  ({type : ACTION_TYPES.TODO_CHANGE_ITEM_TITLE, payload : value});
+export const changeItemTitle = payload => 
+  ({type : ACTION_TYPES.TODO_CHANGE_ITEM_TITLE, payload : payload});
   
 export const addItem = () => 
   ({ type: ACTION_TYPES.TODO_ADD_ITEM });
 
-export const deleteItem = id => 
-  ({ type: ACTION_TYPES.TODO_DELETE_ITEM, payload : id });
+export const deleteItem = payload => 
+  ({ type: ACTION_TYPES.TODO_DELETE_ITEM, payload : payload });
 
-export const changeItemIsDone = id => 
-  ({ type: ACTION_TYPES.TODO_CHANGE_ITEM_IS_DONE, payload : id });
+export const changeItemIsDone = payload => 
+  ({ type: ACTION_TYPES.TODO_CHANGE_ITEM_IS_DONE, payload : payload });
 
